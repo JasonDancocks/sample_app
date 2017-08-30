12c13ffbdac29a75d071a436f373015431e9c16d
Jason Dancocks <jason.dancocks@gmail.com>
2017-08-30 10:49:53 +0100

    add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,27 @@
 class User < ApplicationRecord
-has_many :microposts, dependent: :destroy
+  has_many :microposts, dependent: :destroy
   has_many :active_relationships,  class_name:  "Relationship",
-                                   foreign_key: "follower_id",
-                                   dependent:   :destroy
+  foreign_key: "follower_id",
+  dependent:   :destroy
   has_many :passive_relationships, class_name:  "Relationship",
-                                   foreign_key: "followed_id",
-                                   dependent:   :destroy
+  foreign_key: "followed_id",
+  dependent:   :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   
-	attr_accessor :remember_token, :activation_token, :reset_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
-	before_save   :downcase_email
-	before_create :create_activation_digest
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
-	validates :name, presence: true, length: {maximum: 50}
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, presence: true, length: {maximum: 255}, 
-	format: { with: VALID_EMAIL_REGEX },
-	uniqueness: {case_sensitive: false}
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, 
+  format: { with: VALID_EMAIL_REGEX },
+  uniqueness: {case_sensitive: false}
 
-	has_secure_password
-	validates :password, presence: true, length: {minimum: 6 }, allow_nil: true
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6 }, allow_nil: true
 
 	  # Returns the hash digest of the given string.
 	  def User.digest(string)
@@ -60,7 +60,7 @@ has_many :microposts, dependent: :destroy
   	UserMailer.account_activation(self).deliver_now
   end
 
-   def create_reset_digest
+  def create_reset_digest
     self.reset_token = User.new_token
     update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
@@ -69,12 +69,15 @@ has_many :microposts, dependent: :destroy
     UserMailer.password_reset(self).deliver_now
   end
 
-   def password_reset_expired?
+  def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
 
-  def feed
-  	Micropost.where("user_id = ?", id)
+   def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   def follow(other_user)
